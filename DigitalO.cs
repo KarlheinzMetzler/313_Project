6b6993bd91b60447617d440cf6defb7e8f5abd88
KarlheinzMetzler <Karlheinz@wifi-172-23-48-140.uoa-wifi.auckland.ac.nz>
2016-09-27 10:57:29 +1300

    did nothing

--- DigitalO.cs
+++ DigitalO.cs
@@ -12,16 +12,16 @@ namespace Lab
 		Task digitalOut = new Task();
 		//Create a new single channel writer
 		DigitalSingleChannelWriter writer;
-		
+
 		public void OpenChannel(string portLine, string portName)
 		{
 			//Create a new digital output channel called "Doutport0"
 			digitalOut.DOChannels.CreateChannel("dev1/port0", "DigitalChn0", ChannelLineGrouping.OneChannelForAllLines);
-			
+
 			//Initialise the single channel writer and assign stream of channel to it
 			writer = new DigitalSingleChannelWriter(digitalOut.Stream);
 		}
-		
+
 		public void WriteData(int length)
 		{
 			//call WriteSingleSamplePort method to write data to the channel
