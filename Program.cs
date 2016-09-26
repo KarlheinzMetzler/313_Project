e27dac0f3fbd5b7691461dac1099e275b930fe7d
CallumQuin <callum_quin@hotmail.com>
2016-09-26 18:20:20 +1300

    Added Program.cs Code

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.text;
+
+namespace Lab
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DigitalO dout = new DigitalO();
+			dOut.openChannel();
+			dOut.WriteData(0); // 0: fan/heater off, 1: fan on, 2: heater on, 3: fan/heater on
+		}
+	}
+}
